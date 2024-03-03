78ec45a6ce8f29cec2eccc9836f0037d02c26a9d
juhaj77 <juhaj358@gmail.com>
2024-03-03 19:41:03 +0200

    0-indexed month

--- javascript.js
+++ javascript.js
@@ -216,7 +216,7 @@ const printLocalTime = dateString => {
 
 const stringToDate = s => new Date(
   s.slice(0,4),
-  s.slice(5,7),
+  Number(s.slice(5,7)) - 1,
   s.slice(8,10),
   s.slice(11,13),
   s.slice(14,16),
